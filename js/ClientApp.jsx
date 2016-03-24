923674cb74c9fa766d334f8c7b3ce3d3d929701d
Shanna Sullivan <shanna.sullivan@gmail.com>
2016-03-24 13:48:30 -0700

    filter functionality working, no errors

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -2,14 +2,17 @@ const React = require('react')
 const ReactDOM = require('react-dom')
 const Landing = require('./Landing')
 const Search = require('./Search')
+const Layout = require('./Layout')
 // gets us ReactRouter.router, ReactRouter.route, and ReactRouter.hashHistory
-const { Router, Route, hashHistory } = require('react-router')
+const { Router, Route, IndexRoute, hashHistory } = require('react-router')
 
 // one individual route, uses component as the view/display
 const App = () => (
   <Router history={hashHistory}>
-    <Route path='/' component={Landing} />
-    <Route path='/search' component={Search} />
+    <Route path='/' component={Layout} >
+      <IndexRoute component={Landing} />
+      <Route path='/search' component={Search} />
+    </Route>
   </Router>
 )
 
--- js/Landing.jsx
+++ js/Landing.jsx
@@ -6,12 +6,10 @@ const Landing = () => (
     // to add styling
     // div({style: {color: "red"}},
     // capital MyTitle means use element I created
-  <div className='app-container'>
-    <div className='home-info'>
-      <h1 className='title'>svideo</h1>
-      <input className='search' type='text' placeholder='Search'/>
-      <Link to='/search' className='browse-all'> or Browse All</Link>
-    </div>
+  <div className='home-info'>
+    <h1 className='title'>svideo</h1>
+    <input className='search' type='text' placeholder='Search'/>
+    <Link to='/search' className='browse-all'> or Browse All</Link>
   </div>
 )
 

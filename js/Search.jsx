0232131db2ea8f26be4f8a95869862c80f9fa198
Shanna Sullivan <shanna.sullivan@gmail.com>
2016-03-24 12:36:51 -0700

    cards showing up, all errors gone

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,11 +1,14 @@
 const React = require('react')
+const ShowCard = require('./ShowCard')
 const data = require('../public/data')
 
 const Search = () => (
-  <div className='container' >
-    {data.shows.map((show) => {
-      return <h3>{show.title}</h3>
-    })}
+  <div className='container'>
+    <div className='shows'>
+      {data.shows.map((show) => {
+        return <ShowCard show={show} key={show.imdbID}/>
+      })}
+    </div>
   </div>
 )
 
